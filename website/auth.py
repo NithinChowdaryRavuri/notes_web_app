0ee569f5372aecdf79f247ef53d289a900565f55
“Nithin <ravurinithinchowdary@gmail.com>
2023-10-23 20:30:04 -0500

    Redirected logout page to Base page.

--- website/auth.py
+++ website/auth.py
@@ -29,7 +29,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return render_template('first.html', user=current_user)
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
